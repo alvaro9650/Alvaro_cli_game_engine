825dd60cc01cda374a1d8e67253370a1b9806b0c
alvaro9650 <alumno1718_2@192.168.144.234>
2017-10-23 13:31:20 +0200

    Allow multiple fields in the same game engine

--- src/Game_engine/Game_engine.java
+++ src/Game_engine/Game_engine.java
@@ -10,9 +10,7 @@ package Game_engine;
  * @author alumno1718_2
  */
 public class Game_engine {
-    Field playing_field;
-    Game_engine(Field playing_field){
-        this.playing_field = playing_field;
+    Game_engine(){
     }
     public void DeprecatedDrawFrame(Field field, Ball[] balls, Player[] players) {
         for (int y = 0; y <= field.y_size; y++) {
@@ -36,12 +34,12 @@ public class Game_engine {
         }
         System.out.append("\n");
     }
-    public void DrawFrame(){
+    public void DrawFrame(Field field){
         StringBuilder frame = new StringBuilder();
         char draw_character = '|';
         Integer character_height = 0;
-        for(int y = 0;y<this.playing_field.game_objects[0].length;y++){
-            for (Game_object[][] x : this.playing_field.game_objects) {
+        for(int y = 0;y<field.game_objects[0].length;y++){
+            for (Game_object[][] x : field.game_objects) {
                 for (Game_object game_object : x[y]) {
                     if ( game_object!=null&&game_object.height!=null&&game_object.height>character_height && game_object.height>0 ) {
                         character_height = game_object.height;
@@ -55,6 +53,29 @@ public class Game_engine {
             frame.append("\n");
         }
         System.out.println(frame.toString());
+    }
+    public void DrawFrame(Field[] fields){
+        Integer character_height = 0;
+        StringBuilder frame = new StringBuilder();
+        for ( Field field : fields ){
+            char draw_character = '|';
+            for(int y = 0;y<field.game_objects[0].length;y++){
+                for (Game_object[][] x : field.game_objects) {
+                    for (Game_object game_object : x[y]) {
+                        if ( game_object!=null&&game_object.height!=null&&game_object.height>character_height && game_object.height>0 ) {
+                            character_height = game_object.height;
+                            draw_character = game_object.character;
+                        }
+                    }
+                    frame.append(draw_character);
+                    draw_character = '|';
+                    character_height = 0;
+                }
+                frame.append("\n");
+            }
+            System.out.println(frame.toString());
+            frame.delete(0, frame.length());
+        }
     }
     public void DeprecatedUpdateLocations(Field field, Ball[] balls, Player[] players) {
         for (int y = 0; y <= field.y_size; y++) {
@@ -78,8 +99,8 @@ public class Game_engine {
         }
         System.out.append("\n");
     }
-    public void UpdateLocations() {
-        for ( Game_object[][] x : this.playing_field.game_objects ){
+    public void UpdateLocations(Field field) {
+        for ( Game_object[][] x : field.game_objects ){
             for ( Game_object[] y : x ){
                 for ( Game_object game_object : y ) {
                     if ( game_object!=null ) {
@@ -89,10 +110,15 @@ public class Game_engine {
             }
         }
     }
+    public void UpdateLocations(Field[] fields) {
+        for ( Field field : fields ){
+            this.UpdateLocations(field);
+        }
+    }
     public char ToDrawAt(Field field,Integer x,Integer y){
         char draw_character = ' ';
         Integer character_height = 0;
-        for (Game_object game_object : this.playing_field.game_objects[x][y]) {
+        for (Game_object game_object : field.game_objects[x][y]) {
             if ( game_object!=null&&game_object.height!=null&&game_object.height>character_height && game_object.height>0 ) {
                 character_height = game_object.height;
                 draw_character = game_object.character;
