09a550a5fd9c297257b084e21af2afc2bf04246f
alvaro9650 <alumno1718_2@192.168.144.234>
2017-10-23 14:17:49 +0200

    Object for saving resolutions

---
+++ src/Game_engine/Resolution.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game_engine;
+
+/**
+ * A resolution has a horizontal resolution and vertical resolution
+ *
+ * @author alumno1718_2
+ */
+public class Resolution {
+
+    Integer x;
+    Integer y;
+
+    /**
+     * Creates a resolution
+     *
+     * @param x Horizontal resolution
+     * @param y Vertical resolution
+     */
+    public Resolution(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
