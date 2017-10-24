b3f83bfabc570f7eb46278db702ee852b5cb0fc5
alvaro9650 <alumno1718_2@192.168.144.234>
2017-10-24 08:16:34 +0200

    Add coments and use Resolution object instead of separate Integer variables for resolution

--- src/Game_engine/Camera.java
+++ src/Game_engine/Camera.java
@@ -6,24 +6,35 @@
 package Game_engine;
 
 /**
- *
+ * The camera has a location and a resolution and it's located in a field , it can draw frames
  * @author alumno1718_2
  */
 public class Camera extends Game_object {
 
-    Integer x_resolution;
-    Integer y_resolution;
+    Resolution resolution;
 
-    public Camera(Field field, Integer x, Integer y) {
+    /**
+     * Creates a camera
+     * @param field The field that will be drawedby the camera
+     * @param x_location The horizontal position where the camera is going to be located
+     * @param y_location The vertical position where the camera is going to be located
+     * @param x_resolution Horizontal resolution of the camera
+     * @param y_resolution Vertical resolution of the camera
+     */
+    public Camera(Field field, Integer x_location, Integer y_location,Integer x_resolution,Integer y_resolution) {
         super(field);
-        this.location.x = x;
-        this.location.y = y;
+        this.location.x = x_location;
+        this.location.y = y_location;
+        this.resolution = new Resolution(x_resolution,y_resolution);
     }
 
+    /**
+     * Draws a frame form this camera
+     */
     public void DrawFrame() {
         StringBuilder frame = new StringBuilder();
-        for (int y = this.location.y - y_resolution / 2; y < this.location.y + y_resolution - y_resolution / 2; y++) {
-            for (int x = this.location.x - x_resolution / 2; y < this.location.x + x_resolution - x_resolution / 2; x++) {
+        for (int y = this.location.y - this.resolution.y / 2; y < this.location.y + this.resolution.y - this.resolution.y / 2; y++) {
+            for (int x = this.location.x - this.resolution.x / 2; y < this.location.x + this.location.x - this.location.x / 2; x++) {
                 frame.append((y >= 0 && x >= 0 && x < this.playing_field.x_size && y < this.playing_field.y_size) ? new Game_engine().ToDrawAt(this.playing_field, x, y) : ' ');
             }
             frame.append("\n");
