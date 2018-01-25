18af90846af98f523752808484e68b6c42804260
alvaro9650 <alumno1718_2@192.168.144.234>
2018-01-25 10:11:35 +0100

    Rename getter

---
+++ src/Game_engine/OutOfBoundsMoveType.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game_engine;
+
+/**
+ * Move types to use when it's going out of bounds
+ *
+ * @author alumno1718_2
+ */
+public enum OutOfBoundsMoveType {
+    Bounceable(1), Respawnable(2), Impossible(3), Destroyable(4), Possible(5), Farest(6), Circular_universe(7);
+    private final int out_of_bounds_move_type;
+
+    /**
+     * Constructor for Out_of_bounds_move_type
+     *
+     * @param out_of_bounds_move_type An int that identifies the type
+     */
+    private OutOfBoundsMoveType(int out_of_bounds_move_type) {
+        this.out_of_bounds_move_type = out_of_bounds_move_type;
+    }
+
+    /**
+     * Getter for OutOfBoundsMoveType
+     *
+     * @return Returns an int that identifies the type
+     */
+    public int getOutOfBoundsMoveType() {
+        return this.out_of_bounds_move_type;
+    }
+}
