48bfce64d08ad1cc60fa649ef51d7f51f5cf840b
alvaro9650 <alumno1718_2@192.168.144.234>
2018-01-26 08:27:02 +0100

    Rename variables and functions to comply with CamelCase notation

--- src/Game_engine/Camera.java
+++ src/Game_engine/Camera.java
@@ -20,38 +20,38 @@ public class Camera extends GameObject {
      * Creates a camera
      *
      * @param field The field that will be drawedby the camera
-     * @param x_location The horizontal position where the camera is going to be
+     * @param locationx The horizontal position where the camera is going to be
      * located
-     * @param y_location The vertical position where the camera is going to be
+     * @param locationy The vertical position where the camera is going to be
      * located
-     * @param x_resolution Horizontal resolution of the camera
-     * @param y_resolution Vertical resolution of the camera
+     * @param resolutionx Horizontal resolution of the camera
+     * @param resolutiony Vertical resolution of the camera
      */
-    public Camera(Field field, Integer x_location, Integer y_location, Integer x_resolution, Integer y_resolution) {
+    public Camera(Field field, Integer locationx, Integer locationy, Integer resolutionx, Integer resolutiony) {
         super(field);
-        this.location.x = x_location;
-        this.location.y = y_location;
-        this.resolution = new Resolution(x_resolution, y_resolution);
+        this.location.x = locationx;
+        this.location.y = locationy;
+        this.resolution = new Resolution(resolutionx, resolutiony);
     }
 
     /**
      * Updates the frame of this camera
      */
-    public void UpdateFrame() {
-        StringBuilder frame_creator = new StringBuilder();
+    public void updateFrame() {
+        StringBuilder framecreator = new StringBuilder();
         for (int y = this.location.y - this.resolution.y / 2; y < this.location.y + this.resolution.y - this.resolution.y / 2; y++) {
             for (int x = this.location.x - this.resolution.x / 2; y < this.location.x + this.location.x - this.location.x / 2; x++) {
-                frame_creator.append((y >= 0 && x >= 0 && x < this.playing_field.x_size && y < this.playing_field.y_size) ? new GameEngine().ToDrawAt(this.playing_field, x, y) : ' ');
+                framecreator.append((y >= 0 && x >= 0 && x < this.playing_field.x_size && y < this.playing_field.y_size) ? new GameEngine().ToDrawAt(this.playing_field, x, y) : ' ');
             }
-            frame_creator.append("\n");
+            framecreator.append("\n");
         }
-        this.frame = frame_creator.toString();
+        this.frame = framecreator.toString();
     }
 
     /**
      * Draws the frame of this camera
      */
-    public void DrawFrame() {
+    public void drawFrame() {
         System.out.println(this.frame);
     }
 }
