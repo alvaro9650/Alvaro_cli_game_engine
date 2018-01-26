83533c1032b9a2b326e1973d9b5e04b1aabc5665
alvaro9650 <alumno1718_2@192.168.144.234>
2018-01-26 09:19:08 +0100

    New class for saving 2D sizes

---
+++ src/Game_engine/TwoDimensionsSize.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game_engine;
+
+/**
+ *
+ * @author alumno1718_2
+ */
+public class TwoDimensionsSize {
+
+    Integer x;
+    Integer y;
+
+    /**
+     * Creates an object to save a 2D size
+     *
+     * @param x X size
+     * @param y Y size
+     */
+    public TwoDimensionsSize(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
