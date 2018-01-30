6f75e62d654d2bf7849b01f33c783a40e8e6d7fc
alvaro9650 <alumno1718_2@192.168.144.234>
2018-01-30 09:08:18 +0100

    Set static

--- src/alvaro_tools/Comparators.java
+++ src/alvaro_tools/Comparators.java
@@ -10,196 +10,196 @@ package alvaro_tools;
  * @author alumno1718_2
  */
 public class Comparators {
-    public boolean between(Integer number_tocheck,Integer low_number,Integer high_number){
+    public static boolean between(Integer number_tocheck,Integer low_number,Integer high_number){
         return low_number<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Integer number_tocheck,Integer low_number,Long high_number){
+    public static boolean between(Integer number_tocheck,Integer low_number,Long high_number){
         return low_number.longValue()<number_tocheck.longValue() && number_tocheck.longValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Integer low_number,Float high_number){
+    public static boolean between(Integer number_tocheck,Integer low_number,Float high_number){
         return low_number.floatValue()<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Integer low_number,Double high_number){
+    public static boolean between(Integer number_tocheck,Integer low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Long low_number,Integer high_number){
+    public static boolean between(Integer number_tocheck,Long low_number,Integer high_number){
         return low_number<number_tocheck.longValue() && number_tocheck.longValue()<high_number.longValue();
     }
-    public boolean between(Integer number_tocheck,Long low_number,Long high_number){
+    public static boolean between(Integer number_tocheck,Long low_number,Long high_number){
         return low_number<number_tocheck.longValue() && number_tocheck.longValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Long low_number,Float high_number){
+    public static boolean between(Integer number_tocheck,Long low_number,Float high_number){
         return low_number.floatValue()<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Long low_number,Double high_number){
+    public static boolean between(Integer number_tocheck,Long low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Float low_number,Integer high_number){
+    public static boolean between(Integer number_tocheck,Float low_number,Integer high_number){
         return low_number<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number.floatValue();
     }
-    public boolean between(Integer number_tocheck,Float low_number,Long high_number){
+    public static boolean between(Integer number_tocheck,Float low_number,Long high_number){
         return low_number<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number.floatValue();
     }
-    public boolean between(Integer number_tocheck,Float low_number,Float high_number){
+    public static boolean between(Integer number_tocheck,Float low_number,Float high_number){
         return low_number<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Float low_number,Double high_number){
+    public static boolean between(Integer number_tocheck,Float low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Integer number_tocheck,Double low_number,Integer high_number){
+    public static boolean between(Integer number_tocheck,Double low_number,Integer high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Integer number_tocheck,Double low_number,Long high_number){
+    public static boolean between(Integer number_tocheck,Double low_number,Long high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Integer number_tocheck,Double low_number,Float high_number){
+    public static boolean between(Integer number_tocheck,Double low_number,Float high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Integer number_tocheck,Double low_number,Double high_number){
+    public static boolean between(Integer number_tocheck,Double low_number,Double high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Integer low_number,Integer high_number){
+    public static boolean between(Long number_tocheck,Integer low_number,Integer high_number){
         return low_number.longValue()<number_tocheck && number_tocheck<high_number.longValue();
     }
-    public boolean between(Long number_tocheck,Integer low_number,Long high_number){
+    public static boolean between(Long number_tocheck,Integer low_number,Long high_number){
         return low_number.longValue()<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Long number_tocheck,Integer low_number,Float high_number){
+    public static boolean between(Long number_tocheck,Integer low_number,Float high_number){
         return low_number.floatValue()<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Integer low_number,Double high_number){
+    public static boolean between(Long number_tocheck,Integer low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Long low_number,Integer high_number){
+    public static boolean between(Long number_tocheck,Long low_number,Integer high_number){
         return low_number<number_tocheck && number_tocheck<high_number.longValue();
     }
-    public boolean between(Long number_tocheck,Long low_number,Long high_number){
+    public static boolean between(Long number_tocheck,Long low_number,Long high_number){
         return low_number<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Long number_tocheck,Long low_number,Float high_number){
+    public static boolean between(Long number_tocheck,Long low_number,Float high_number){
         return low_number.floatValue()<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Long low_number,Double high_number){
+    public static boolean between(Long number_tocheck,Long low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Float low_number,Integer high_number){
+    public static boolean between(Long number_tocheck,Float low_number,Integer high_number){
         return low_number<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number.floatValue();
     }
-    public boolean between(Long number_tocheck,Float low_number,Long high_number){
+    public static boolean between(Long number_tocheck,Float low_number,Long high_number){
         return low_number<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number.floatValue();
     }
-    public boolean between(Long number_tocheck,Float low_number,Float high_number){
+    public static boolean between(Long number_tocheck,Float low_number,Float high_number){
         return low_number<number_tocheck.floatValue() && number_tocheck.floatValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Float low_number,Double high_number){
+    public static boolean between(Long number_tocheck,Float low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Long number_tocheck,Double low_number,Integer high_number){
+    public static boolean between(Long number_tocheck,Double low_number,Integer high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Long number_tocheck,Double low_number,Long high_number){
+    public static boolean between(Long number_tocheck,Double low_number,Long high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Long number_tocheck,Double low_number,Float high_number){
+    public static boolean between(Long number_tocheck,Double low_number,Float high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Long number_tocheck,Double low_number,Double high_number){
+    public static boolean between(Long number_tocheck,Double low_number,Double high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Float number_tocheck,Integer low_number,Integer high_number){
+    public static boolean between(Float number_tocheck,Integer low_number,Integer high_number){
         return low_number.floatValue()<number_tocheck && number_tocheck<high_number.floatValue();
     }
-    public boolean between(Float number_tocheck,Integer low_number,Long high_number){
+    public static boolean between(Float number_tocheck,Integer low_number,Long high_number){
         return low_number.floatValue()<number_tocheck && number_tocheck<high_number.floatValue();
     }
-    public boolean between(Float number_tocheck,Integer low_number,Float high_number){
+    public static boolean between(Float number_tocheck,Integer low_number,Float high_number){
         return low_number.floatValue()<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Float number_tocheck,Integer low_number,Double high_number){
+    public static boolean between(Float number_tocheck,Integer low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Float number_tocheck,Long low_number,Integer high_number){
+    public static boolean between(Float number_tocheck,Long low_number,Integer high_number){
         return low_number.floatValue()<number_tocheck && number_tocheck<high_number.floatValue();
     }
-    public boolean between(Float number_tocheck,Long low_number,Long high_number){
+    public static boolean between(Float number_tocheck,Long low_number,Long high_number){
         return low_number.floatValue()<number_tocheck && number_tocheck<high_number.floatValue();
     }
-    public boolean between(Float number_tocheck,Long low_number,Float high_number){
+    public static boolean between(Float number_tocheck,Long low_number,Float high_number){
         return low_number.floatValue()<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Float number_tocheck,Long low_number,Double high_number){
+    public static boolean between(Float number_tocheck,Long low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Float number_tocheck,Float low_number,Integer high_number){
+    public static boolean between(Float number_tocheck,Float low_number,Integer high_number){
         return low_number<number_tocheck && number_tocheck<high_number.floatValue();
     }
-    public boolean between(Float number_tocheck,Float low_number,Long high_number){
+    public static boolean between(Float number_tocheck,Float low_number,Long high_number){
         return low_number<number_tocheck && number_tocheck<high_number.floatValue();
     }
-    public boolean between(Float number_tocheck,Float low_number,Float high_number){
+    public static boolean between(Float number_tocheck,Float low_number,Float high_number){
         return low_number<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Float number_tocheck,Float low_number,Double high_number){
+    public static boolean between(Float number_tocheck,Float low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Float number_tocheck,Double low_number,Integer high_number){
+    public static boolean between(Float number_tocheck,Double low_number,Integer high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Float number_tocheck,Double low_number,Long high_number){
+    public static boolean between(Float number_tocheck,Double low_number,Long high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Float number_tocheck,Double low_number,Float high_number){
+    public static boolean between(Float number_tocheck,Double low_number,Float high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number.doubleValue();
     }
-    public boolean between(Float number_tocheck,Double low_number,Double high_number){
+    public static boolean between(Float number_tocheck,Double low_number,Double high_number){
         return low_number<number_tocheck.doubleValue() && number_tocheck.doubleValue()<high_number;
     }
-    public boolean between(Double number_tocheck,Integer low_number,Integer high_number){
+    public static boolean between(Double number_tocheck,Integer low_number,Integer high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Integer low_number,Long high_number){
+    public static boolean between(Double number_tocheck,Integer low_number,Long high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Integer low_number,Float high_number){
+    public static boolean between(Double number_tocheck,Integer low_number,Float high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Integer low_number,Double high_number){
+    public static boolean between(Double number_tocheck,Integer low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Double number_tocheck,Long low_number,Integer high_number){
+    public static boolean between(Double number_tocheck,Long low_number,Integer high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Long low_number,Long high_number){
+    public static boolean between(Double number_tocheck,Long low_number,Long high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Long low_number,Float high_number){
+    public static boolean between(Double number_tocheck,Long low_number,Float high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Long low_number,Double high_number){
+    public static boolean between(Double number_tocheck,Long low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Double number_tocheck,Float low_number,Integer high_number){
+    public static boolean between(Double number_tocheck,Float low_number,Integer high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Float low_number,Long high_number){
+    public static boolean between(Double number_tocheck,Float low_number,Long high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Float low_number,Float high_number){
+    public static boolean between(Double number_tocheck,Float low_number,Float high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Float low_number,Double high_number){
+    public static boolean between(Double number_tocheck,Float low_number,Double high_number){
         return low_number.doubleValue()<number_tocheck && number_tocheck<high_number;
     }
-    public boolean between(Double number_tocheck,Double low_number,Integer high_number){
+    public static boolean between(Double number_tocheck,Double low_number,Integer high_number){
         return low_number<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Double low_number,Long high_number){
+    public static boolean between(Double number_tocheck,Double low_number,Long high_number){
         return low_number<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Double low_number,Float high_number){
+    public static boolean between(Double number_tocheck,Double low_number,Float high_number){
         return low_number<number_tocheck && number_tocheck<high_number.doubleValue();
     }
-    public boolean between(Double number_tocheck,Double low_number,Double high_number){
+    public static boolean between(Double number_tocheck,Double low_number,Double high_number){
         return low_number<number_tocheck && number_tocheck<high_number;
     }
 }
