2198753d37cdad3c3a1f7e04b9a58c75c8f9363c
alvaro9650 <alumno1718_2@192.168.144.234>
2018-02-02 09:14:56 +0100

    Add class for saving 3d sizes

---
+++ src/Game_engine/ThreeDimensionsSize.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game_engine;
+
+/**
+ *
+ * @author alvaro9650
+ */
+public class ThreeDimensionsSize {
+
+    public Integer x;
+    public Integer y;
+    public Integer z;
+
+    /**
+     * Creates an object to save a 3D size
+     *
+     * @param x X size
+     * @param y Y size
+     * @param z Z size
+     * @author alvaro9650
+     */
+    public ThreeDimensionsSize(Integer x, Integer y,Integer z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
