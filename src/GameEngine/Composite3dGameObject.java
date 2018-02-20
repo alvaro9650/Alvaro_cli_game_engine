3b2ad0668d75714ef103fc75cefb6a2089d28e32
alvaro9650 <alumno1718_2@192.168.144.234>
2018-02-20 08:36:28 +0100

    Add necessary things for constructor

--- src/GameEngine/Composite3dGameObject.java
+++ src/GameEngine/Composite3dGameObject.java
@@ -12,10 +12,21 @@ package GameEngine;
 public class Composite3dGameObject extends GameObject {
 
     public ThreeDimensionsSize size;
+    public GameObject[][][][] objectcomponents;
 
-    public Composite3dGameObject(Field field) {
+    /**
+     * Costructor for Composite3dGameObject
+     *
+     * @param field The field where the object is located
+     * @param x X size of the object
+     * @param y Y size of the object
+     * @param z Z size of the object
+     * @param maxobjectspercoord Maximum number of objects per object coord
+     * @author alvaro9650
+     */
+    public Composite3dGameObject(Field field, Integer x, Integer y, Integer z, Integer maxobjectspercoord) {
         super(field);
-        size = new ThreeDimensionsSize(1, 1, 1);
+        size = new ThreeDimensionsSize(x, y, z);
+        objectcomponents = new GameObject[x][y][z][maxobjectspercoord];
     }
-
 }
