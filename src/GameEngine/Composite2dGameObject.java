d03b79ef51a2b050435514f597fbb17d64284f17
alvaro9650 <alumno1718_2@192.168.144.234>
2018-02-20 09:18:49 +0100

    Add code for adding an object component or deleting it

--- src/GameEngine/Composite2dGameObject.java
+++ src/GameEngine/Composite2dGameObject.java
@@ -5,6 +5,8 @@
  */
 package GameEngine;
 
+import java.util.Objects;
+
 /**
  *
  * @author alvaro9650
@@ -12,7 +14,7 @@ package GameEngine;
 public class Composite2dGameObject extends CompositeObject {
 
     public TwoDimensionsSize size;
-    public GameObject[][][] objectcomponents;
+    public GameObject[][][] componentobjects;
 
     /**
      * Costructor for Composite2dGameObject
@@ -26,7 +28,92 @@ public class Composite2dGameObject extends CompositeObject {
     public Composite2dGameObject(Field field, Integer x, Integer y, Integer maxobjectspercoord) {
         super(field);
         size = new TwoDimensionsSize(x, y);
-        objectcomponents = new GameObject[x][y][maxobjectspercoord];
+        componentobjects = new GameObject[x][y][maxobjectspercoord];
     }
 
+    /**
+     * Add an object to a location in the object
+     *
+     * @param gameobject The object you want to add
+     * @throws GameEngine.ImpossibleLocationAddException
+     * @throws GameEngine.ObjectCollidesException
+     * @throws GameEngine.OutOfBoundsException
+     * @throws GameEngine.ImpossibleLocationRemoveException
+     * @author alvaro9650
+     */
+    public void addGameObject(GameObject gameobject) throws ImpossibleLocationAddException, ObjectCollidesException, OutOfBoundsException, ImpossibleLocationRemoveException {
+        GameObject collider;
+        if (gameobject.located) {
+            this.deleteGameObject(gameobject);
+        }
+        if (!(new RectangularArea(this.size.x - 1, 0, this.size.y - 1, 0).getCommonArea(gameobject.posiblelocationarea).isInside(gameobject.location))) {
+            throw new OutOfBoundsException();
+        } else if ((collider = this.collidesWith(gameobject)) != null) {
+            switch (collider.physicalstatetype) {
+                case Solid:
+                case SolidWithHoles:
+                    switch (gameobject.physicalstatetype) {
+                        case SolidWithHoles:
+                        case Solid:
+                            throw new ObjectCollidesException();
+                    }
+                case Liquid:
+                    switch (gameobject.physicalstatetype) {
+                        case Solid:
+                            throw new ObjectCollidesException();
+                    }
+                case Gas:
+                    switch (gameobject.physicalstatetype) {
+                        case Solid:
+                        case Liquid:
+                            throw new ObjectCollidesException();
+                    }
+            }
+        }
+        Integer i = 0;
+        while (i < this.componentobjects[gameobject.location.x][gameobject.location.y].length && this.componentobjects[gameobject.location.x][gameobject.location.y][i] != null) {
+            i++;
+        }
+        if (i < this.componentobjects[gameobject.location.x][gameobject.location.y].length) {
+            this.componentobjects[gameobject.location.x][gameobject.location.y][i] = gameobject;
+            gameobject.arrayposition = i;
+        } else {
+            throw new ImpossibleLocationAddException("No space available");
+        }
+        gameobject.located = true;
+    }
+
+    /**
+     * Remove an object from a location in the object
+     *
+     * @param gameobject The object you want to delete
+     * @throws GameEngine.ImpossibleLocationRemoveException
+     * @author alvaro9650
+     */
+    public void deleteGameObject(GameObject gameobject) throws ImpossibleLocationRemoveException {
+        if (gameobject.arrayposition < this.componentobjects[gameobject.location.x][gameobject.location.y].length && gameobject.hashCode() == this.componentobjects[gameobject.location.x][gameobject.location.y][gameobject.arrayposition].hashCode()) {
+            this.componentobjects[gameobject.location.x][gameobject.location.y][gameobject.arrayposition] = null;
+        } else {
+            throw new ImpossibleLocationRemoveException("Object is not in the object stated location");
+        }
+        gameobject.located = false;
+    }
+
+    /**
+     * Checks if an object collides with something in that location
+     *
+     * @param objectlookingforcollider The object that you want to know if
+     * collides with something
+     * @return The object to collide with or null if it doesn't collide with
+     * anything
+     * @author alvaro9650
+     */
+    public GameObject collidesWith(GameObject objectlookingforcollider) {
+        for (GameObject object : this.componentobjects[objectlookingforcollider.location.x][objectlookingforcollider.location.y]) {
+            if (object != null && Objects.equals(object.height, objectlookingforcollider.height) && !Objects.equals(object, objectlookingforcollider)) {
+                return object;
+            }
+        }
+        return null;
+    }
 }
