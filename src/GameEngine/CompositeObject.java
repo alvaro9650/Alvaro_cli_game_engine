0c181f23e2d00575d12947d49abbc1aa257cadb0
alvaro9650 <alumno1718_2@192.168.144.234>
2018-02-20 08:59:32 +0100

    Set initial object

---
+++ src/GameEngine/CompositeObject.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameEngine;
+
+/**
+ *
+ * @author alvaro9650
+ */
+public class CompositeObject extends GameObject{
+    
+    public CompositeObject(Field field) {
+        super(field);
+    }
+    
+}
