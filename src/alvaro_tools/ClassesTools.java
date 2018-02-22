8bcacc2d0028daea4212687ec508af787e4f3f6a
alvaro9650 <alumno1718_2@192.168.144.234>
2018-02-22 11:53:17 +0100

    Add more tools

---
+++ src/alvaro_tools/ClassesTools.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alvaro_tools;
+
+/**
+ *
+ * @author alvaro9650
+ */
+public class ClassesTools {
+    public static Object NVL(Object nullable,Object value){
+        return nullable==null ? value : nullable;
+    }
+}
