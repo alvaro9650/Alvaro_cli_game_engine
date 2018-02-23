8781fdd2370f92ed979ca4624e6b3936083cc798
alvaro9650 <alumno1718_2@192.168.144.234>
2018-02-23 08:18:08 +0100

    Add type for snake parts

---
+++ src/Snake/SnakePartType.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Snake;
+
+/**
+ * Snake part types
+ *
+ * @author alvaro9650
+ */
+public enum SnakePartType {
+    HEAD, BODY, TAIL;
+    private final int snakeparttype;
+
+    /**
+     * Constructor for SnakePartType
+     *
+     * @param snakeparttype An int that identifies the part of the snake body
+     * @author alvaro9650
+     */
+    private SnakePartType(int snakeparttype) {
+        this.snakeparttype = snakeparttype;
+    }
+
+    /**
+     * Getter for SnakePartType
+     *
+     * @return Returns an int that identifies the part of the snake body
+     * @author alvaro9650
+     */
+    public int getSnakePartType() {
+        return this.snakeparttype;
+    }
+}
