c8acfbf2cc9901d8f5300b01bd4981df511faaeb
alvaro9650 <alumno1718_2@192.168.144.234>
2018-04-27 11:30:21 +0200

    Fix bug:null pointer exception when using body

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -39,6 +39,7 @@ public class Snake extends Composite2dGameObject {
         this.tail = new SnakeComponent(this);
         this.tail.part = SnakePartType.HEAD;
         this.tail.location = new Coordinate(4, 5);
+        body = new ArrayList<>();
         try {
             this.addComposite2dGameObjectComponent(head);
         } catch (ImpossibleLocationAddException ex) {
