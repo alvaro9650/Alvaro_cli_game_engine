cf26ebc6a0fc19ecae0f5b2d02d41f0e6ddf165c
alvaro9650 <alumno1718_2@192.168.144.234>
2018-05-07 13:39:19 +0200

    Add code to process when colliding with an apple

--- src/Snake/SnakeComponent.java
+++ src/Snake/SnakeComponent.java
@@ -6,6 +6,7 @@
 package Snake;
 
 import GameEngine.Composite2dGameObjectComponent;
+import GameEngine.GameObject;
 import GameEngine.MoveType;
 
 /**
@@ -22,5 +23,26 @@ public class SnakeComponent extends Composite2dGameObjectComponent {
         this.movetype = MoveType.Teleport;
         this.height = 1;
         this.objecttype = "SnakePart";
+    }
+    /**
+     * Function to add custom actions when colliding against a determined object
+     * or object type
+     *
+     * @param collisionreceiver The object that receives the collision
+     * @author alvaro9650
+     */
+    @Override
+    public void giveCollision(GameObject collisionreceiver) {
+        switch (collisionreceiver.objectidentifier) {
+            default:
+                break;
+        }
+        switch (collisionreceiver.objecttype) {
+            case "Apple":
+                ((Snake)parent).eatApple();
+                break;
+            default:
+                break;
+        }
     }
 }
